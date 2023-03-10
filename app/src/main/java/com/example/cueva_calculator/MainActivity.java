bf5f8d19d3e136da597fde41ceedcaba7abbe417
raffi1 <rafaelluiscueva545@gmail.com>
2023-03-10 21:23:15 +0800

    Cueva_Calculator

--- app/src/main/java/com/example/cueva_calculator/MainActivity.java
+++ app/src/main/java/com/example/cueva_calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cueva_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void zeroBTN (View view){
+
+    }
+public void oneBTN(View view){
+
+}
+
+public void pointBTN(View view){
+
+}
+
+    public void equalsBTN(View view){
+
+    }
+
+    public void plusMinusBTN(View view){
+
+    }
+    public void twoBTN(View view){
+
+    }
+    public void threeBTN(View view){
+
+    }public void plusBTN(View view){
+
+    }
+    public void fourBTN (View view){
+
+    }
+    public void fiveBTN (View view){
+
+    }
+    public void sixBTN (View view){
+
+    }
+    public void minusBTN (View view){
+
+    }
+    public void sevenBTN (View view){
+
+    }
+    public void eightBTN(View view){
+
+    }
+    public void nineBTN(View view){
+
+    }
+
+    public void multiplyBTN(View view){
+
+    }
+    public void clearBTN (View view){
+
+    }
+
 }
